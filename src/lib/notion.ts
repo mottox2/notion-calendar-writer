281f44b3e0a6362504dbaeaa3e96488cf09d4b3e
mottox2 <y.mottox2@gmail.com>
2021-09-24 18:11:58 +0900

    Add notion codes

---
+++ src/lib/notion.ts
@@ -0,0 +1,49 @@
+import { Client } from "@notionhq/client";
+
+const notion = new Client({ auth: process.env.NOTION_KEY });
+const databaseId = process.env.NOTION_DATABASE_ID;
+
+export const createCalendarItem = async (
+  databaseId: string,
+  title: string,
+  start: string,
+  end?: string
+) => {
+  const res = await notion.pages.create({
+    parent: { database_id: databaseId },
+    properties: {
+      Name: {
+        type: "title",
+        title: [
+          {
+            type: "text",
+            text: {
+              content: title,
+            },
+          },
+        ],
+      },
+      Date: {
+        type: "date",
+        date: { start, end },
+      },
+    },
+  });
+  console.log(`Page is created: ${res.url}`);
+};
+
+export const getCalendarItems = async (
+  databaseId: string,
+  before: string,
+  after: string
+) => {
+  const res = await notion.databases.query({
+    database_id: databaseId,
+    filter: {
+      property: "Date",
+      date: { before, after },
+    },
+  });
+
+  console.log(`Result: ${res.results.length} items.`);
+};
